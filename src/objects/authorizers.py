ceaa66ee134a1c5f4d168ad87b028e79905f2599
Sergey <128322450+CraZZy67@users.noreply.github.com>
2025-03-02 00:48:11 +0300

    Создал класс аунтефикации.
    Пока без отлова ошибок и тестирования.

---
+++ src/objects/authorizers.py
@@ -0,0 +1,62 @@
+from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver.common.by import By
+from dotenv import load_dotenv
+
+import pickle
+import os
+
+
+
+class UserAuthorizer:
+    LOGIN_LINK = 'https://login.vk.com/?act=web_token'
+    MESSENGER_LINK = 'https://vk.com/im'
+    LOCAL_STORAGE_KEY = '6287487:web_token:login:auth'
+    
+    def __init__(self, headless: bool = True):
+        options = ChromeOptions()
+        
+        if headless:
+            options.add_argument('--headless')
+            
+        self.driver = Chrome(options=options)
+        self.driver.implicitly_wait(30.0)
+    
+    def send_verify_code(self, phone_number: str):
+        self.driver.get(self.MESSENGER_LINK)
+        
+        button = self.driver.find_element(By.CSS_SELECTOR, '[data-testid="enter-another-way"]')
+        button.click()
+        
+        div = self.driver.find_element(By.CLASS_NAME, 'vkuiFormField__content')
+        div.find_element(By.TAG_NAME, 'input').send_keys(phone_number)
+        
+        button = self.driver.find_element(By.CSS_SELECTOR, '[data-test-id="submit_btn"]')
+        button.click()
+    
+    def enter_verify_code(self, verify_code: str):
+        splited_code = list(verify_code)
+        cells_input = self.driver.find_elements(By.NAME, 'otp-cell')
+        
+        for number, cell in zip(splited_code, cells_input):
+            cell.send_keys(number)
+    
+    def enter_password(self, password: str):
+        div = self.driver.find_element(By.CLASS_NAME, 'vkc__Password__Wrapper')
+        div.find_element(By.NAME, 'password').send_keys(password)
+        
+        div = self.driver.find_element(By.CLASS_NAME, 'vkc__EnterPasswordNoUserInfo__buttonWrap')
+        div.find_element(By.TAG_NAME, 'button').click
+    
+    def save_session_creds(self):
+        self.driver.get(self.LOGIN_LINK)
+        
+        cookie = self.driver.get_cookies()
+        creds = self.driver.execute_script(f'return localStorage.getItem("{self.LOCAL_STORAGE_KEY}");')
+        
+        load_dotenv()
+        creds_path = os.getenv('CREDS_PATH')
+        
+        with open(creds_path + 'user_creds.pickle', 'wb') as file:
+            pickle.dump(dict(cookie=cookie, access_token=creds['access_token']), file)
+
+# ANONYM_LINK = 'https://vk.com/icollbelgu'
