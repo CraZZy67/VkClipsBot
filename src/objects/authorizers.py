aa3fc3fe5138012748a1bf0ba6cf1b4398411903
Sergey <128322450+CraZZy67@users.noreply.github.com>
2025-03-03 23:33:34 +0300

    Протестировал класс и исправил ошибки
    Есть проблема с капчей и отловом ошибок

--- src/objects/authorizers.py
+++ src/objects/authorizers.py
@@ -1,10 +1,11 @@
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
-from dotenv import load_dotenv
 
 import pickle
-import os
+from time import sleep
 
+from src.settings import Settings
+from src.logger import logger
 
 
 class UserAuthorizer:
@@ -45,18 +46,21 @@ class UserAuthorizer:
         div.find_element(By.NAME, 'password').send_keys(password)
         
         div = self.driver.find_element(By.CLASS_NAME, 'vkc__EnterPasswordNoUserInfo__buttonWrap')
-        div.find_element(By.TAG_NAME, 'button').click
+        div.find_element(By.TAG_NAME, 'button').click()
+        
+        sleep(4.0)
     
-    def save_session_creds(self):
+    def save_session_creds(self, creds_path: str = Settings.CREDS_PATH):
+        creds = self.driver.execute_script(
+            f'return JSON.parse(localStorage.getItem("{self.LOCAL_STORAGE_KEY}"));')
         self.driver.get(self.LOGIN_LINK)
         
+        sleep(2.0)
+        
         cookie = self.driver.get_cookies()
-        creds = self.driver.execute_script(f'return localStorage.getItem("{self.LOCAL_STORAGE_KEY}");')
         
-        load_dotenv()
-        creds_path = os.getenv('CREDS_PATH')
+        logger.debug(f'Пойманые Cookie: {[x['name'] for x in cookie]}')
+        logger.debug(f'Пойманный токен: {creds}')
         
-        with open(creds_path + 'user_creds.pickle', 'wb') as file:
+        with open(creds_path + 'user_creds.pkl', 'wb') as file:
             pickle.dump(dict(cookie=cookie, access_token=creds['access_token']), file)
-
-# ANONYM_LINK = 'https://vk.com/icollbelgu'
