75fd9c5617c5949868c0e61ea680c323efc0b425
Sergey <128322450+CraZZy67@users.noreply.github.com>
2025-03-18 23:40:58 +0300

    Написал тесты для класса видео очереди и протестировал класс

--- src/objects/video_queue.py
+++ src/objects/video_queue.py
@@ -1,11 +1,13 @@
 from collections import deque
 from asyncio import sleep
+import time
 
 
 from src.objects.uploader import Uploader
 from src.my_exceptions import QueueLenException
 
-class VideoQueue:
+
+class DebugVideoQueue:
     UPLOADER = Uploader()
     
     def __init__(self, inter_public: str, own_public: str, interval: int):
@@ -42,3 +44,23 @@ class VideoQueue:
             self.queue.pop()
         else:
             raise QueueLenException
+
+
+class DebugVideoQueue(DebugVideoQueue):
+    def run_next_video(self):
+        if self.run: 
+            if len(self.queue): 
+                time.sleep(float(self.interval * 60))
+            else:
+                raise QueueLenException
+            
+        if self.run:
+            if len(self.queue):
+                video_id = self.queue.pop()
+                
+                self.UPLOADER.upload(self.own_public, 
+                                    self.inter_public, video_id=video_id)
+            else:
+                raise QueueLenException
+        else:
+            self.run = True
---
+++ tests/test_video_queue.py
@@ -0,0 +1,59 @@
+import unittest
+import dotenv
+
+import os
+
+from src.objects.video_queue import DebugVideoQueue
+from src.my_exceptions import QueueLenException
+
+
+class TestVideoQueue(unittest.TestCase):
+    def test_main(self):
+        dotenv.load_dotenv()
+        
+        ex = DebugVideoQueue(os.getenv('PUBLIC_ID_FOR_TEST'),
+                        os.getenv('OWN_TEST_PUBLIC_ID'), 1)
+        
+        ex.add_video(os.getenv('VIDEO_ID_FOR_TEST'))
+        ex.add_video(os.getenv('VIDEO_ID_FOR_TEST'))
+        self.assertEqual(len(ex.queue), 2)
+        
+        ex.run_next_video()
+        
+        ex.delete_video()
+        self.assertEqual(len(ex.queue), 0)
+    
+    def test_stop(self):
+        dotenv.load_dotenv()
+        
+        ex = DebugVideoQueue(os.getenv('PUBLIC_ID_FOR_TEST'),
+                        os.getenv('OWN_TEST_PUBLIC_ID'), 1)
+        
+        ex.add_video(os.getenv('VIDEO_ID_FOR_TEST'))
+        
+        ex.run = False
+        ex.run_next_video()
+        
+        print('Остановленно')
+    
+    def test_error(self):
+        dotenv.load_dotenv()
+        
+        ex = DebugVideoQueue(os.getenv('PUBLIC_ID_FOR_TEST'),
+                        os.getenv('OWN_TEST_PUBLIC_ID'), 1)
+        
+        with self.assertRaises(QueueLenException):
+            ex.run_next_video()
+        with self.assertRaises(QueueLenException):
+            ex.delete_video()
+    
+    def test_len(self):
+        dotenv.load_dotenv()
+        
+        ex = DebugVideoQueue(os.getenv('PUBLIC_ID_FOR_TEST'),
+                        os.getenv('OWN_TEST_PUBLIC_ID'), 1)
+        
+        [ex.add_video(os.getenv('VIDEO_ID_FOR_TEST')) for x in range(8)]
+        
+        self.assertEqual(len(ex.queue), 5)
+        
